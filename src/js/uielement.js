3670e7c828015b4ecf229dc24cd6269a05cc5b3d
aungkyawzaww <aungkyawzaww7889@gmail.com>
2025-01-14 23:15:54 +0630

    public chat room

---
+++ src/js/uielement.js
@@ -0,0 +1,38 @@
+import { format } from "date-fns";
+
+export function UiElement(divele){
+
+    const userInfoEle = (data)=>{
+
+        const uid = data.uid;
+        const email = data.email;
+        const fullname = data.displayName;
+        const photourl = data.photoURL;
+        const createdtime = data.metadata.creationTime;
+
+        const getdate = new Date(createdtime).getDate();
+        const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+        const getmonth = new Date(createdtime).getMonth();
+        const getyear = new Date(createdtime).getFullYear();
+
+        // const formatteddate = `${getdate} ${months[getmonth]} ${getyear}`;
+        // console.log(formatteddate);
+
+
+        const formatteddate = format(new Date(createdtime),"do MMM yyyy");
+
+        const html = `
+            <img src="${photourl}" width="80" alt="profile icon" />
+            <p>UID : ${uid}</p>
+            <p>Display Name : ${fullname}</p>
+            <p>Email : ${email}</p>
+            <p>Create At : ${formatteddate}</p>
+        `;
+
+        divele.innerHTML = html;
+    }
+
+    // you must include {}
+    return {userInfoEle};
+
+}
